e40c5550af23b9bbe882539af2bd585a8ddc46a1
Afnan Abbas Khan <afnanniaxi@gmail.com>
2025-05-22 13:30:30 +0500

    connecting the database for user login

---
+++ app/login.php
@@ -0,0 +1,3 @@
+<?php
+echo"Welcome to the Task Management System";
+?>
--- login.php
+++ login.php
@@ -9,12 +9,13 @@
     <link rel="stylesheet" href="css/lstye.css">
 </head>
 <body class="login-body bg-light" style="min-height:100vh;">
+
 <div class="d-flex justify-content-center align-items-center" style="min-height:100vh;">
-    <form class="shadow p-4 bg-white rounded" style="min-width:350px; width:100%; max-width:450px;">
+    <form  method="POST" action="app/login.php" class="shadow p-4 bg-white rounded" style="min-width:350px; width:100%; max-width:450px;">
         <h3 class="display-4">LOGIN</h3>
         <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <label for="exampleInputEmail1" class="form-label">User name</label>
+            <input type="text" class="form-control">
         </div>
         <div class="mb-3">
             <label for="exampleInputPassword1" class="form-label">Password</label>
@@ -23,6 +24,6 @@
         <button type="submit" class="btn btn-primary">Login</button>
     </form>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bund le.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
-</html>
+</html> 
